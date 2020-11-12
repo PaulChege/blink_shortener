cecd1b5e0970e453b97e9f36943acfcdb5d7ddf4
Paul <pchegenjenga@gmail.com>
2020-11-12 10:04:17 +0300

    First commit

---
+++ lib/blink_shortener.rb
@@ -0,0 +1,6 @@
+require "blink_shortener/version"
+
+module BlinkShortener
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/blink_shortener/version.rb
@@ -0,0 +1,3 @@
+module BlinkShortener
+  VERSION = "0.1.0"
+end
